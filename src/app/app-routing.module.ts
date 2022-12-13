6c1ca33dd9d98a3e2f3fbdc0ef0e3d953d342977
Locit2003 <110243167+Locit2003@users.noreply.github.com>
2022-12-13 16:35:09 +0700

    angular views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CandidateDetailComponent } from './components/candidate-detail/candidate-detail.component';
+import { CandidateComponent } from './components/candidate/candidate.component';
+import { ContestDetailComponent } from './components/contest-detail/contest-detail.component';
+import { ContestsComponent } from './components/contests/contests.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contests', component: ContestsComponent},
+  { path: 'contest-detail', component: ContestDetailComponent},
+  { path: 'candidates', component: CandidateComponent},
+  { path: 'candidate-detail', component: CandidateDetailComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'user-profile', component: UserProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContestsComponent } from './components/contests/contests.component';
+import { ContestDetailComponent } from './components/contest-detail/contest-detail.component';
+import { CandidateComponent } from './components/candidate/candidate.component';
+import { CandidateDetailComponent } from './components/candidate-detail/candidate-detail.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContestsComponent,
+    ContestDetailComponent,
+    CandidateComponent,
+    CandidateDetailComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserProfileComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
